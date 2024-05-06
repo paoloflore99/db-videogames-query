32fa45ad81c39c6a9d35a6b269c537fa459e1c4a
paolo flore <paoloflore937@gmail.com>
2024-05-06 14:15:20 +0200

    Aggiungere i file di progetto.

---
+++ db-videogames-query/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace db_videogames_query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
